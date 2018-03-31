b6bb8abecdeb21d9aa96c6b3096031a4b4d20728
Michael Rondon <mrondon72@gmail.com>
2018-03-30 20:04:46 -0400

    implementacion de relaciones entre modelos

--- app/Transaction.php
+++ app/Transaction.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Buyer;
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
@@ -12,5 +14,15 @@ class Transaction extends Model
     	'buyer_id',
     	'product_id',
     ];
+
+    public function buyer()
+    {
+    	return $this->belongsTo(Buyer::class);
+    }
+
+    public function product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 
 }
