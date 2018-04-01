4ba054d1e6999468c628543081cf8aac4783d249
Michael Rondon <mrondon72@gmail.com>
2018-04-01 14:38:09 -0400

    factory de product

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\Product;
 use App\Category;
 use Faker\Generator as Faker;
 
@@ -31,5 +32,17 @@ $factory->define(Category::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
+        'image' => $faker->randomElement(['1.jpg', '2.png', '3.jpg']),
+        // 'seller_id' => User::inRandomOrder()->first()->id,
+        'seller_id' => User::all()->random()->id,
     ];
 });
