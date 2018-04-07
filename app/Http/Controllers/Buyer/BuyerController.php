a8b33b982ceb8269e9005cb7c278adbe2599f373
Michael Rondon <mrondon72@gmail.com>
2018-04-06 23:47:48 -0400

    mostrar un comprador

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -29,7 +29,11 @@ class BuyerController extends Controller
      */
     public function show($id)
     {
-        //
+        
+        $comprador = Buyer::has('transactions')->findOrFail( $id );
+
+        return response()->json(['data' => $comprador], 200);
+        
     }
 
 }
