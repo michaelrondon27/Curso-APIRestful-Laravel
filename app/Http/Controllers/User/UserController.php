27d014f6151c41a27a1723854b4107c3acc6ddae
Michael Rondon <mrondon72@gmail.com>
2018-04-07 18:46:09 -0400

    inyeccion implicita de modelos

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -57,12 +57,10 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
 
-        $usuario = User::findOrFail($id);
-
-        return $this->showOne( $usuario );
+        return $this->showOne( $user );
 
     }
 
@@ -73,10 +71,8 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-
-        $user = User::findOrFail($id);
 
         $rules = [
             'email' => 'email|unique:users,email,' . $user->id,
@@ -136,10 +132,8 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        
-        $user = User::findOrFail($id);
 
         $user->delete();
 
