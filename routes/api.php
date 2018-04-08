2f28d25e6462ad39029631a3e5529a6d6f8db467
Michael Rondon <mrondon72@gmail.com>
2018-04-07 23:10:44 -0400

    controlador para obtener las categorias de las transacciones

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::resource('products', 'Product\ProductController', ['only' => ['index', 's
 	Transactions
 */
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
 
 /*
 	Sellers
