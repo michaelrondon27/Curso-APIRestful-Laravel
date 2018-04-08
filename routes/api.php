2234c2f8d604b48a226eb2fd08a1fad4524a67b9
Michael Rondon <mrondon72@gmail.com>
2018-04-08 11:49:11 -0400

    controlador category seller

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['onl
 */
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
 Route::resource('categories.products', 'Category\CategoryProductController', ['only' => ['index']]);
+Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => ['index']]);
 
 /*
 	Products
