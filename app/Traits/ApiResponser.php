bb9399392e4c643ea882a30053d0c59c87782903
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-04-09 14:47:44 -0400

    implementar sistema de verificacion

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -8,24 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 trait ApiResponser
 {
 
-	private function successRespose( $data, $code )
+	private function successRespose($data, $code)
 	{
 		return response()->json( $data, $code );
 	}
 
-	protected function errorResponse( $message, $code )
+	protected function errorResponse($message, $code)
 	{
 		return response()->json( ['error' => $message, 'code' => $code], $code );
 	}
 
-	protected function showAll( Collection $collection, $code = 200 )
+	protected function showAll(Collection $collection, $code = 200)
 	{
 		return $this->successRespose( ['data' => $collection], $code );
 	}
 
-	protected function showOne( Model $instance, $code = 200 )
+	protected function showOne(Model $instance, $code = 200)
 	{
 		return $this->successRespose( ['data' => $instance], $code );
+	}
+
+	public function showMessage($message, $code = 200)
+	{
+		return $this->successRespose( ['data' => $message], $code );
 	}
 
 }
