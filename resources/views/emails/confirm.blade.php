82c686766b0a7ee355896ea506d43a716404fa99
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-04-09 15:53:11 -0400

    usando los markdown  en los mailable

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,4 +1,12 @@
-Hola {{ $user->name }}
-Has cambiado tu correo lectrónico. Por favor verifica la nueva dirección usando el siguiente enlace:
+@component('mail::message')
+# Hola {{ $user->name }}
 
-{{ route('verify', $user->verification_token) }}
+Has cambiado tu correo lectrónico. Por favor verifica la nueva dirección usando el siguiente botón:
+
+@component('mail::button', ['url' => route('verify', $user->verification_token)])
+Confiirmar mi cuenta
+@endcomponent
+
+Gracias,<br>
+{{ config('app.name') }}
+@endcomponent
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -2,3 +2,16 @@ Hola {{ $user->name }}
 Gracias por crear una cuenta. Por favor verifícala usando el siguiente enlace:
 
 {{ route('verify', $user->verification_token) }}
+
+@component('mail::message')
+# Hola {{ $user->name }}
+
+Gracias por crear una cuenta. Por favor verifícala usando el siguiente botón:
+
+@component('mail::button', ['url' => route('verify', $user->verification_token)])
+Confiirmar mi cuenta
+@endcomponent
+
+Gracias,<br>
+{{ config('app.name') }}
+@endcomponent
