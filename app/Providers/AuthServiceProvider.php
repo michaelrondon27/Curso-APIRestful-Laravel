b24b927666f71c22c7aec512f791b9b2e1e219e9
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-04-10 17:16:00 -0400

    corrigiendo error con tokensExpireIn

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
-        Passport::tokenExpireIn(Carbon::now()->addMinutes(30));
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
     }
 }
