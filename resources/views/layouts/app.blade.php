0cf867ec4f393929af76ff2fe0e31b3ae449d884
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-04-11 14:46:31 -0400

    componente para gestionar clientes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,9 +37,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         @if (Auth::check())
-                            <li>
-                                <a href="{{ route('personal-tokens') }}">My Tokens</a>
-                            </li>
+                            <li><a href="{{ route('personal-tokens') }}">My Tokens</a></li>
+                            <li><a href="{{ route('personal-clients') }}">My Clients</a></li>
+                            <li><a href="{{ route('authorized-clients') }}">Authorized Clients</a></li>
                         @endif
                     </ul>
 
--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,6 @@ $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
+Route::get('/home/my-clients', 'HomeController@getClients')->name('personal-clients');
+Route::get('/home/authorized-clients', 'HomeController@getAuthorizedClients')->name('authorized-clients');
 Route::get('/home', 'HomeController@index')->name('home');
