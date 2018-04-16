91dc4ba2988cdbf56439a4d9f49ccfad4816e91d
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-04-16 16:20:25 -0400

    permitiendo cors en respuestas de error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use App\Traits\ApiResponser;
+use Asm89\Stack\CorsService;
 use Illuminate\Database\QueryException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Session\TokenMismatchException;
@@ -60,7 +61,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        $response = $this->handleException($request, $exception);
 
+        app(CorsService::class)->addActualRequestHeaders($response, $request);
+
+        return $response;
+
+    }
+
+    public function handleException($request, Exception $exception)
+    {
         if ( $exception instanceof ModelNotFoundException ) {
 
             $modelo = strtolower( class_basename( $exception->getModel() ) );
@@ -123,8 +133,6 @@ class Handler extends ExceptionHandler
         }
 
         return $this->errorResponse( 'Falla inesperada. Intente Luego', 500 );
-
-
     }
 
     private function isFrontend($request)
